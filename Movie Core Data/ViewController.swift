8ab2f7fa88c5126eb16540c862819721da395777
ahmedpro <ahmedmuhammedpro@gmail.com>
2020-04-01 10:29:55 +0200

    Initial Commit

---
+++ Movie Core Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Core Data
+//
+//  Created by ahmedpro on 4/1/20.
+//  Copyright © 2020 ahmedpro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
